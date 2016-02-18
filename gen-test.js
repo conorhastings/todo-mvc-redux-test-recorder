e65b4c597b4a891d582b1247c3a6770bc5d58292
Conor Hastings <hastings.conorm@gmail.com>
2016-02-17 23:57:53 -0500

    update to latest version of test recorder for nicer test output, update gen-test-example

--- gen-test.js
+++ gen-test.js
@@ -1,8 +1,8 @@
 var test = require('tape');
 require('babel-register');
 var equal = require('deep-equal');
-
 var reducer = require('./reducers').default;
+
 var state = {
     "todos": [
         {
@@ -19,34 +19,109 @@ test('expected state returned for each action', function(assert) {
   var actions = [
   {
     "action": {
-      "type": "COMPLETE_TODO",
-      "id": 4
+      "type": "ADD_TODO",
+      "text": "no really use redux"
+    },
+    "nextState": {
+      "todos": [
+        {
+          "id": 1,
+          "completed": false,
+          "text": "no really use redux"
+        },
+        {
+          "text": "Use Redux",
+          "completed": false,
+          "id": 0
+        }
+      ]
+    }
+  },
+  {
+    "action": {
+      "type": "ADD_TODO",
+      "text": "seriously use redux"
+    },
+    "nextState": {
+      "todos": [
+        {
+          "id": 2,
+          "completed": false,
+          "text": "seriously use redux"
+        },
+        {
+          "id": 1,
+          "completed": false,
+          "text": "no really use redux"
+        },
+        {
+          "text": "Use Redux",
+          "completed": false,
+          "id": 0
+        }
+      ]
+    }
+  },
+  {
+    "action": {
+      "type": "ADD_TODO",
+      "text": "then add redux test recorder"
+    },
+    "nextState": {
+      "todos": [
+        {
+          "id": 3,
+          "completed": false,
+          "text": "then add redux test recorder"
+        },
+        {
+          "id": 2,
+          "completed": false,
+          "text": "seriously use redux"
+        },
+        {
+          "id": 1,
+          "completed": false,
+          "text": "no really use redux"
+        },
+        {
+          "text": "Use Redux",
+          "completed": false,
+          "id": 0
+        }
+      ]
+    }
+  },
+  {
+    "action": {
+      "type": "ADD_TODO",
+      "text": "then sleep well"
     },
     "nextState": {
       "todos": [
         {
           "id": 4,
           "completed": false,
-          "text": "great"
+          "text": "then sleep well"
         },
         {
           "id": 3,
-          "completed": true,
-          "text": "awesome"
+          "completed": false,
+          "text": "then add redux test recorder"
         },
         {
           "id": 2,
-          "completed": true,
-          "text": "dope"
+          "completed": false,
+          "text": "seriously use redux"
         },
         {
           "id": 1,
-          "completed": true,
-          "text": "sweet"
+          "completed": false,
+          "text": "no really use redux"
         },
         {
           "text": "Use Redux",
-          "completed": true,
+          "completed": false,
           "id": 0
         }
       ]
@@ -55,64 +130,29 @@ test('expected state returned for each action', function(assert) {
   {
     "action": {
       "type": "COMPLETE_TODO",
-      "id": 3
+      "id": 0
     },
     "nextState": {
       "todos": [
         {
           "id": 4,
           "completed": false,
-          "text": "great"
+          "text": "then sleep well"
         },
         {
           "id": 3,
           "completed": false,
-          "text": "awesome"
-        },
-        {
-          "id": 2,
-          "completed": true,
-          "text": "dope"
-        },
-        {
-          "id": 1,
-          "completed": true,
-          "text": "sweet"
-        },
-        {
-          "text": "Use Redux",
-          "completed": true,
-          "id": 0
-        }
-      ]
-    }
-  },
-  {
-    "action": {
-      "type": "COMPLETE_TODO",
-      "id": 2
-    },
-    "nextState": {
-      "todos": [
-        {
-          "id": 4,
-          "completed": false,
-          "text": "great"
-        },
-        {
-          "id": 3,
-          "completed": false,
-          "text": "awesome"
+          "text": "then add redux test recorder"
         },
         {
           "id": 2,
           "completed": false,
-          "text": "dope"
+          "text": "seriously use redux"
         },
         {
           "id": 1,
-          "completed": true,
-          "text": "sweet"
+          "completed": false,
+          "text": "no really use redux"
         },
         {
           "text": "Use Redux",
@@ -132,22 +172,22 @@ test('expected state returned for each action', function(assert) {
         {
           "id": 4,
           "completed": false,
-          "text": "great"
+          "text": "then sleep well"
         },
         {
           "id": 3,
           "completed": false,
-          "text": "awesome"
+          "text": "then add redux test recorder"
         },
         {
           "id": 2,
           "completed": false,
-          "text": "dope"
+          "text": "seriously use redux"
         },
         {
           "id": 1,
-          "completed": false,
-          "text": "sweet"
+          "completed": true,
+          "text": "no really use redux"
         },
         {
           "text": "Use Redux",
@@ -160,33 +200,33 @@ test('expected state returned for each action', function(assert) {
   {
     "action": {
       "type": "COMPLETE_TODO",
-      "id": 0
+      "id": 2
     },
     "nextState": {
       "todos": [
         {
           "id": 4,
           "completed": false,
-          "text": "great"
+          "text": "then sleep well"
         },
         {
           "id": 3,
           "completed": false,
-          "text": "awesome"
+          "text": "then add redux test recorder"
         },
         {
           "id": 2,
-          "completed": false,
-          "text": "dope"
+          "completed": true,
+          "text": "seriously use redux"
         },
         {
           "id": 1,
-          "completed": false,
-          "text": "sweet"
+          "completed": true,
+          "text": "no really use redux"
         },
         {
           "text": "Use Redux",
-          "completed": false,
+          "completed": true,
           "id": 0
         }
       ]
@@ -195,44 +235,73 @@ test('expected state returned for each action', function(assert) {
   {
     "action": {
       "type": "COMPLETE_TODO",
+      "id": 3
+    },
+    "nextState": {
+      "todos": [
+        {
+          "id": 4,
+          "completed": false,
+          "text": "then sleep well"
+        },
+        {
+          "id": 3,
+          "completed": true,
+          "text": "then add redux test recorder"
+        },
+        {
+          "id": 2,
+          "completed": true,
+          "text": "seriously use redux"
+        },
+        {
+          "id": 1,
+          "completed": true,
+          "text": "no really use redux"
+        },
+        {
+          "text": "Use Redux",
+          "completed": true,
+          "id": 0
+        }
+      ]
+    }
+  },
+  {
+    "action": {
+      "type": "DELETE_TODO",
       "id": 4
     },
     "nextState": {
       "todos": [
         {
-          "id": 4,
-          "completed": true,
-          "text": "great"
-        },
-        {
           "id": 3,
-          "completed": false,
-          "text": "awesome"
+          "completed": true,
+          "text": "then add redux test recorder"
         },
         {
           "id": 2,
-          "completed": false,
-          "text": "dope"
+          "completed": true,
+          "text": "seriously use redux"
         },
         {
           "id": 1,
-          "completed": false,
-          "text": "sweet"
+          "completed": true,
+          "text": "no really use redux"
         },
         {
           "text": "Use Redux",
-          "completed": false,
+          "completed": true,
           "id": 0
         }
       ]
     }
   }
 ];
-  var returnExpectedState = actions.map(function (action) {
+  actions.forEach(function(action, index) {
     var result = reducer(state, action.action);
     state = result;
-    return equality(result, action.nextState);
+    assert.ok(equality(result, action.nextState), action.action.type + '(action index ' + index +') correctly updated state');
   });
-  assert.ok(returnExpectedState.every(function(expected) { return expected }), 'expected state returned for each action');
   assert.end();
 });
